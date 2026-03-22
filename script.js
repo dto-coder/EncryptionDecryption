f590e78ed0f6918cd4f53b1cc701a31c39c71710
dto <tdenny6136@gmail.com>
2026-03-21 21:38:02 -0400

    Add functionality to generate and display basic components of RSA

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function appendToDisplay() {
     var display = document.getElementById("display");
     display.innerHTML = input;
 }
+function generateRandomPrime() {
+    const primes = [];
+    for (let num = 2; primes.length < 2**10; num++) {
+        let isPrime = true;
+        for (let j = 2; j <= Math.sqrt(num); j++) {
+            if (num % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(num);
+        }
+    }
+    const randomIndex = Math.floor(Math.random() * primes.length);
+    const randomPrime = primes[randomIndex];
+    document.getElementById("display").innerHTML = randomPrime;
+}
